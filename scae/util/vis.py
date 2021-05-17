cdd244cb722f4259c3eaf4bf38499b5a5ac0ebd6
axquaris <dominykas@berkeley.edu>
2021-05-17 11:49:51 -0700

    resonator scae updates

--- scae/util/vis.py
+++ scae/util/vis.py
@@ -1,4 +1,6 @@
 import torch
+import torchshow as ts
+
 import matplotlib.pyplot as plt
 from functools import partial
 import numpy as np
@@ -15,34 +17,39 @@ def show_img(tensor, min=0, max=1):
     plt.show()
 
 
-def plot_image_tensor(X):
-    plt.figure()
+def plot_image_tensor(X, width=8., titles=None):
     num_imgs = X.shape[0]
     len_y = int(np.floor(np.sqrt(num_imgs)))
     len_x = int(np.ceil(num_imgs / len_y))
 
-    fig, ax = plt.subplots(len_y, len_x)
+    fig, ax = plt.subplots(len_y, len_x, figsize=(width, width / len_x * len_y))
     for i in range(len_y):
         for j in range(len_x):
+            a = ax[i, j] if len_y > 1 else ax[j]
             idx = i * len_x + j
             if idx < num_imgs:
-                ax[i, j].imshow(X[idx][0].detach().cpu().numpy())
-            ax[i, j].get_xaxis().set_visible(False)
-            ax[i, j].get_yaxis().set_visible(False)
-    fig.subplots_adjust(hspace=0.1)  # No horizontal space between subplots
+                a.imshow(X[idx][0].detach().cpu().numpy())
+            a.get_xaxis().set_visible(False)
+            a.get_yaxis().set_visible(False)
+            if titles is not None and i == 0:
+                a.set_title(titles[j])
+    fig.subplots_adjust(hspace=0)  # No horizontal space between subplots
     fig.subplots_adjust(wspace=0)
     fig.show()
 
-def plot_image_tensor_2D(X):
-    plt.figure()
+def plot_image_tensor_2D(X, width=8., titles=None):
     len_y, len_x = X.shape[:2]
-    fig, ax = plt.subplots(len_y, len_x)
+
+    fig, ax = plt.subplots(len_y, len_x, figsize=(width, width / len_x * len_y))
     for i in range(len_y):
         for j in range(len_x):
-            ax[i, j].imshow(X[i, j][0].detach().cpu().numpy())
-            ax[i, j].get_xaxis().set_visible(False)
-            ax[i, j].get_yaxis().set_visible(False)
-    fig.subplots_adjust(hspace=0.1)  # No horizontal space between subplots
+            a = ax[i, j] if len_y > 1 else ax[j]
+            a.imshow(X[i, j][0].detach().cpu().numpy())
+            a.get_xaxis().set_visible(False)
+            a.get_yaxis().set_visible(False)
+            if titles is not None and i == 0:
+                a.set_title(titles[j])
+    fig.subplots_adjust(hspace=0)  # No horizontal space between subplots
     fig.subplots_adjust(wspace=0)
     fig.show()
 
